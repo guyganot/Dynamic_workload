a8530b0d5e09f72d609a6418451c89ecbe8ebf82
yotamgalinka <104307618+yotamgalinka@users.noreply.github.com>
2023-06-10 13:30:48 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,7 +7,6 @@ aws ec2 create-key-pair --key-name $KEY_NAME \
 
 # secure the key pair
 chmod 400 $KEY_PEM
-# ssh -i $KEY_PEM ex1@ip-172-31-27-253.eu-west-1.compute.internal
 
 SEC_GRP="my-sg-`date +'%N'`"
 
@@ -22,36 +21,73 @@ echo "My IP: $MY_IP"
 
 
 echo "Setup rule allowing SSH access to $MY_IP only"
-aws ec2 authorize-security-group-ingress --group-name "$SEC_GRP" --port 22 --protocol tcp --cidr "$MY_IP"/32
+aws ec2 authorize-security-group-ingress --group-name "$SEC_GRP" --port 22 --protocol tcp --source-group "$SEC_GRP" --cidr "$MY_IP"/32
 
 echo "Setup rule allowing HTTP (port 5000) access to all IPs"
-aws ec2 authorize-security-group-ingress --group-name "$SEC_GRP" --port 5000 --protocol tcp --cidr 0.0.0.0/0
+aws ec2 authorize-security-group-ingress --group-name "$SEC_GRP" --port 5000 --protocol tcp --source-group "$SEC_GRP" --cidr "$MY_IP"/32
 
 UBUNTU_20_04_AMI="ami-00aa9d3df94c6c354"
 
-echo "Creating Ubuntu 20.04 instance..."
-RUN_INSTANCES=$(aws ec2 run-instances   \
+echo "Creating Ubuntu 20.04 instance 1..."
+RUN_INSTANCE_1=$(aws ec2 run-instances   \
     --image-id $UBUNTU_20_04_AMI        \
     --instance-type t2.micro            \
     --key-name $KEY_NAME                \
     --security-groups $SEC_GRP)
 
-INSTANCE_ID=$(echo $RUN_INSTANCES | jq -r '.Instances[0].InstanceId')
+INSTANCE_ID_1=$(echo $RUN_INSTANCE_1 | jq -r '.Instances[0].InstanceId')
 
-echo "Waiting for instance creation..."
-aws ec2 wait instance-running --instance-ids $INSTANCE_ID
+echo "Waiting for instance 1 creation..."
+aws ec2 wait instance-running --instance-ids $INSTANCE_ID_1
 
-PUBLIC_IP=$(aws ec2 describe-instances  --instance-ids $INSTANCE_ID | 
+PUBLIC_IP_1=$(aws ec2 describe-instances  --instance-ids $INSTANCE_ID_1 | 
     jq -r '.Reservations[0].Instances[0].PublicIpAddress'
 )
 
-echo "New instance $INSTANCE_ID @ $PUBLIC_IP"
+PRIVATE_IP_1=$(aws ec2 describe-instances --instance-ids "$INSTANCE_ID_1" --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
 
-echo "deploying code to production"
-scp -i $KEY_PEM -o "StrictHostKeyChecking=no" -o "ConnectionAttempts=60" main.py ubuntu@$PUBLIC_IP:/home/ubuntu/
+echo "New instance 1 $INSTANCE_ID_1 @ $PUBLIC_IP_1"
 
-echo "setup production environment"
-ssh -i $KEY_PEM -o "StrictHostKeyChecking=no" -o "ConnectionAttempts=10" ubuntu@$PUBLIC_IP <<EOF
+echo "Creating Ubuntu 20.04 instance 2..."
+RUN_INSTANCE_2=$(aws ec2 run-instances   \
+    --image-id $UBUNTU_20_04_AMI        \
+    --instance-type t2.micro            \
+    --key-name $KEY_NAME                \
+    --security-groups $SEC_GRP)
+
+INSTANCE_ID_2=$(echo $RUN_INSTANCE_2 | jq -r '.Instances[0].InstanceId')
+
+echo "Waiting for instance 2 creation..."
+aws ec2 wait instance-running --instance-ids $INSTANCE_ID_2
+
+PUBLIC_IP_2=$(aws ec2 describe-instances  --instance-ids $INSTANCE_ID_2 | 
+    jq -r '.Reservations[0].Instances[0].PublicIpAddress'
+)
+
+PRIVATE_IP_2=$(aws ec2 describe-instances --instance-ids "$INSTANCE_ID_2" --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
+
+echo "New instance 2 $INSTANCE_ID_2 @ $PUBLIC_IP_2"
+
+AWS_ACCESS_KEY_ID=$(aws configure get aws_access_key_id)
+AWS_SECRET_ACCESS_KEY=$(aws configure get aws_secret_access_key)
+AWS_REGION=$(aws configure get region)
+
+# echo "deploying code to production"
+# scp -i $KEY_PEM -o "StrictHostKeyChecking=no" -o "ConnectionAttempts=60" main.py ubuntu@$PUBLIC_IP:/home/ubuntu/
+
+
+echo "setup production environment for instance 1"
+ssh -i $KEY_PEM -o "StrictHostKeyChecking=no" -o "ConnectionAttempts=10" ubuntu@$PUBLIC_IP_1 <<EOF
+    sudo apt update
+    sudo apt install python3-flask -y
+    export FLASK_APP="main.py"
+    # run app
+    nohup flask run --host 0.0.0.0 &>/dev/null &
+    exit
+EOF
+
+echo "setup production environment for instance 2"
+ssh -i $KEY_PEM -o "StrictHostKeyChecking=no" -o "ConnectionAttempts=10" ubuntu@$PUBLIC_IP_2 <<EOF
     sudo apt update
     sudo apt install python3-flask -y
     export FLASK_APP="main.py"
