c09bce88122494965e1be49b746784ee0ecd315f
yotamgalinka <104307618+yotamgalinka@users.noreply.github.com>
2023-06-20 20:25:13 +0300

    Update worker.py

--- worker.py
+++ worker.py
@@ -5,35 +5,42 @@ import time
 
 app = Flask(__name__)
 
-
 class Worker:
     instance_id = None
     parent_ip = None
     nodesList = []
 
-    @app.route('/start', methods=['PUT'])
+    @staticmethod
     def start():
+        worker = Worker()
+        worker.run()
+
+    def run(self):
+        self.initialize_worker()
+        while self.check_work_time():
+            self.process_nodes()
+            time.sleep(1)
+        self.terminate()
+
+    def initialize_worker(self):
         parent = request.args.get('parentIP')
         machine = request.args.get('machineIP')
-        Worker.instanceId = request.args.get('workerID')
+        Worker.instance_id = request.args.get('workerID')
         Worker.nodesList.append(parent)
         Worker.parent_ip = parent
         if machine is not None:
             Worker.nodesList.append(machine)
-        lastWorkTime = datetime.now()
-        while (datetime.now() - lastWorkTime).total_seconds() < 60:
-            for n in Worker.nodesList:
-                task = Worker.giveMeWork(n)
-                if task['status'] != 0:
-                    result = Worker.work(task['buffer'].encode('utf-8'), int(task['iterations']))
-                    Worker.workDone(n, {'response': result.decode('latin-1')})
-                    lastWorkTime = datetime.now()
-                    continue
-            time.sleep(1)
-        Worker.terminate()
 
+    def check_work_time(self):
+        last_work_time = datetime.now()
+        return (datetime.now() - last_work_time).total_seconds() < 60
 
-
+    def process_nodes(self):
+        for n in Worker.nodesList:
+            task = self.giveMeWork(n)
+            if task['status'] != 0:
+                result = self.work(task['buffer'].encode('utf-8'), int(task['iterations']))
+                self.workDone(n, {'response': result.decode('latin-1')})
 
     @staticmethod
     def work(buffer, iterations):
@@ -49,21 +56,26 @@ class Worker:
         try:
             response = requests.get(url)
             if response.status_code == 200:
-                return response.json()      
+                return response.json()
         except Exception as e:
             print(f"Error: {e}")
         return {"status": 0}
-
 
     @staticmethod
     def workDone(ip, output):
         url = f"http://{ip}:5000/workDone"
         requests.put(url, json=output)
 
-    @staticmethod
-    def terminate():
+    def terminate(self):
         url = f"http://{Worker.parent_ip}:5000/terminate"
         requests.post(url, json={'id': Worker.instance_id})
+
+
+@app.route('/start', methods=['PUT'])
+def start():
+    Worker.start()
+    return 'Worker started.'
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
